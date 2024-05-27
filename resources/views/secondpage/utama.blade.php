b4728dfcbc323ff97d840c7f7eed006c2889f628
miftahsabirah <miftahsabirah@mail.ugm.ac.id>
2024-05-27 21:10:58 +0700

    hal utama

--- resources/views/secondpage/utama.blade.php
+++ resources/views/secondpage/utama.blade.php
@@ -6,11 +6,17 @@
     <div class="m-4">
         <a href="#" class="underline font-regular text-lg">Akses Kode API</a>
 
-        <div class="w-sceen flex flex-wrap justify-center">
-            <p>CARI NIK</p>
-            <button class="bg-yellow-400 p-1 text-center color-white rounded-lg">Tambah Pasien</button>
-        </div>
-        
+        <div class="flex justify-center">
+            <div class="flex flex-wrap justify-between lg:w-1/3 md:w-full">
+                <p class="font-semibold text-3xl">CARI NIK</p>
+                <button class="bg-yellow-400 p-2 px-12 text-center color-white rounded-lg text-sm text-white">Tambah Pasien</button>
+            </div>
+
+            <div class="flex flex-wrap justify-between lg:w-1/3 md:w-full">
+                <p class="font-semibold text-3xl">CARI NIK</p>
+                <button class="bg-yellow-400 p-2 px-12 text-center color-white rounded-lg text-sm text-white">Tambah Pasien</button>
+            </div>
+        </div> 
     </div>
 
 
