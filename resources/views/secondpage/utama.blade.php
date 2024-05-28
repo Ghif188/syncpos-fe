2273f591dfdce6d3ae7aa7b6476db0a4f50fc2a4
Ghif188 <ghifarikahfi162@gmail.com>
2024-05-27 22:51:55 -0400

    tes

--- resources/views/secondpage/utama.blade.php
+++ resources/views/secondpage/utama.blade.php
@@ -13,7 +13,7 @@
             </div>
 
             <div class="flex flex-wrap justify-between lg:w-1/3 md:w-full">
-                <p class="font-semibold text-3xl">CARI NIK</p>
+                <p class="font-semibold text-3xl">CARI NIK </p>
                 <button class="bg-yellow-400 p-2 px-12 text-center color-white rounded-lg text-sm text-white">Tambah Pasien</button>
             </div>
         </div> 
